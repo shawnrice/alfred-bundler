776476b773e337e0154ca9004b98e4d34b4b13a5
deanishe <deanishe@deanishe.net>
2014-06-08 01:20:28 +0200

    Add proper documentation to bundler.py

--- wrappers/bundler.py
+++ wrappers/bundler.py
@@ -3,10 +3,106 @@
 
 
 """
-Python implementation of the Alfred Bundler.
+#########################
+Alfred Bundler for Python
+#########################
 
-NOTE: The installation of Python libraries is currently unfinished due
-to the bundler.sh script it depends on being broken.
+Alfred Bundler is a framework to help workflow authors manage external
+utilities and libraries required by their workflows without having to
+include them with each and every workflow.
+
+`Alfred Bundler Homepage/Documentation <http://shawnrice.github.io/alfred-bundler/>`_.
+
+**NOTE**: By necessity, this Python implementation does not work in
+exactly the same way as the reference PHP/bash implementations by
+Shawn Rice.
+
+The purpose of the Bundler is to enable workflow authors to easily
+access utilites and libraries that are commonly used without having to
+include a copy in every ``.alfredworkflow`` file or worry about installing
+them themselves. This way, we can hopefully avoid having, say, 15 copies
+of ``cocaoDialog`` clogging up users' Dropboxes, and also allow authors to
+distribute workflows with sizeable requirements without their exceeding
+GitHub's 10 MB limit for ``.alfredworkflow`` files or causing authors
+excessive bandwidth costs.
+
+Unfortunately, due to the nature of Python's import system, it isn't
+possible to provide versioned libraries in the way that the PHP Bundler
+does, so this version of the Bundler creates an individual library
+directory for each workflow and adds it to ``sys.path`` with one simple call.
+
+It is based on `Pip <http://pip.readthedocs.org/en/latest/index.html>`_,
+the de facto Python library installer, and you must create a
+``requirements.txt`` file in `the format required by pip <http://pip.readthedocs.org/en/latest/user_guide.html#requirements-files>`_
+in order to take advantage of automatic dependency installation.
+
+Usage
+======
+
+The Python Bundler provides two main features: the ability to use common
+utility programs (e.g. `cocaoDialog <http://mstratman.github.io/cocoadialog/>`_
+or `Pashua <http://www.bluem.net/en/mac/pashua/>`_) simply by asking for
+them by name (they will automatically be installed if necessary), and the
+ability to automatically install and update any Python libraries required
+by your workflows.
+
+Simply include this ``bundler.py`` file (from the Alfred Bundler's ``wrappers``
+directory) alongside your workflow's Python code where it can be imported.
+
+Using utilities/assets
+----------------------
+
+The basic interface for utilities is::
+
+    import bundler
+    util_path = bundler.utility('utilityName')
+
+which will return the path to the appropriate executable, installing
+it first if necessary. You may optionally specify a version number
+and/or your own JSON file that defines a non-standard utility.
+
+Please see `the Alfred Bundler documentation <http://shawnrice.github.io/alfred-bundler/>`_
+for details of the JSON file format and how the Bundler works in general.
+
+Handling Python dependencies
+----------------------------
+
+The Python Bundler can also take care of your workflow's dependencies for
+you if you create a `requirements.txt <http://pip.readthedocs.org/en/latest/user_guide.html#requirements-files>`_
+file in your workflow root directory and put the following in your Python
+source files before trying to import any of those dependencies::
+
+    import bundler
+    bundler.init()
+
+:func:`~bundler.init()` will find your ``requirements.txt``  file (you may alternatively
+specify the path explicitly—see :func:`~bundler.init()`)
+and call Pip with it (installing Pip first if necessary). Then it will
+add the directory it's installed the libraries in to ``sys.path``, so you
+can immediately ``import`` those libraries::
+
+    import bundler
+    bundler.init()
+    import requests  # specified in `requirements.txt`
+
+The Bundler doesn't define any explicit exceptions, but may raise any number
+of different ones (e.g. :class:`~exceptions.IOError` if a file doesn't
+exist or if the computer or PyPi is offline).
+
+By and large, these are not recoverable errors, but if you'd like to ensure
+your workflow's users are notified, I recommend (shameless plug) building
+your Python workflow with
+`Alfred-Workflow <http://www.deanishe.net/alfred-workflow/>`_,
+which can catch workflow errors and warn the user (amongst other cool stuff).
+
+Any problems with the Bundler may be raised on
+`Alfred's forum <http://www.alfredforum.com/topic/4255-alfred-dependency-downloader-framework/>`_
+or on `GitHub <https://github.com/shawnrice/alfred-bundler>`_. **Note:**
+This Python version currently only exists in `this fork <https://github.com/deanishe/alfred-bundler>`_, so it's
+currently better to raise problems specific to the Python version there.
+
+Alfred Bundler Methods
+======================
 
 """
 
@@ -107,11 +203,7 @@ def _add_pip_path():
     assert os.path.exists(PIP_JSON_PATH), ('Error retrieving Pip recipe '
                                            'from GitHub.')
 
-    # This isn't working. `bundler.sh` appears to drop custom JSON
-    # paths on the floor and only work with things specified in
-    # its own `meta/defaults`
     pip_path = utility('Pip', json_path=PIP_JSON_PATH)
-    print('pip_path : {}'.format(pip_path))
 
     sys.path.insert(0, pip_path)
 
@@ -121,8 +213,8 @@ def _bootstrap():
     and install them if not.
 
     NOTE: This will not actuall install the bundler. That will happen the
-    first time `~bundler.utility()`, `~bundler.init()` or
-    `~bundler._load_pip()` is called.
+    first time :func:`~bundler.utility()`, `~bundler.init()` or
+    :func:`~bundler._add_pip_path()` is called.
 
     :returns: ``None``
 
@@ -144,7 +236,20 @@ def _bootstrap():
 
 
 def utility(name, version='default', json_path=None):
-    """Get path to specified utility or asset, installing it if necessary.
+    """Get path to specified utility or asset, installing it first if necessary.
+
+    Use this method to access common command line utilities, such as
+    `cocaoDialog <http://mstratman.github.io/cocoadialog/>`_ or
+    `Terminal-Notifier <https://github.com/alloy/terminal-notifier>`_.
+
+    This function will return the path to the appropriate executable
+    (installing it first if necessary), which you can then utilise via
+    :mod:`subprocess`.
+
+    You can easily add your own utilities by means of JSON configuration
+    files specified with the ``json_path`` argument. Please see
+    `the Alfred Bundler documentation <http://shawnrice.github.io/alfred-bundler/>`_
+    for details of the JSON file format.
 
     :param name: Name of the utility/asset to install
     :type name: ``unicode`` or ``str``
@@ -161,7 +266,7 @@ def utility(name, version='default', json_path=None):
     # Call bash wrapper with specified arguments
     json_path = json_path or ''
     cmd = ['/bin/bash', HELPER_PATH, name, version, 'utility', json_path]
-    path = subprocess.check_output(cmd).strip()
+    path = subprocess.check_output(cmd).strip().decode('utf-8')
     # bundler.sh is broken and returns an error message if something goes
     # wrong instead of exiting uncleanly, so check that path exists, else
     # treat it as an error message
@@ -173,18 +278,26 @@ def utility(name, version='default', json_path=None):
 
 
 def asset(name, version='default', json_path=None):
-    """Synonym for `~bundler.utility()`"""
+    """Synonym for :func:`~bundler.utility()`"""
     return utility(name, version, json_path)
 
 
 def init(requirements=None):
-    """Install dependencies from ``requirements.txt``.
+    """Install dependencies from ``requirements.txt`` to your workflow's
+    custom bundler directory and add this directory to ``sys.path``.
 
-    Will search directory tree for ``requirements.txt`` if ``requirements``
-    is not specified.
+    Will search up the directory tree for ``requirements.txt`` if
+    ``requirements`` argument is not specified.
 
-    :param requirements: Path to ``requirements.txt``
-    :type requirements: ``unicode`` or `str``
+    **Note:** Your workflow must have a bundle ID set in order to use
+    this function. (You should set one anyway, especially if you intend
+    to distribute your workflow.)
+
+    Your ``requirements.txt`` file must be in the
+    `format required by Pip <http://pip.readthedocs.org/en/latest/user_guide.html#requirements-files>`_.
+
+    :param requirements: Path to Pip requirements file
+    :type requirements: ``unicode`` or ``str``
     :returns: ``None``
 
     """
