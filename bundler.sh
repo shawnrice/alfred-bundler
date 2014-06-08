85caeabeee348d6de6cbd2406134acd7be49d13b
Shawn Rice <rice.shawn@gmail.com>
2014-06-08 14:20:49 -0400

    added gatekeeper check only for .app utilities

--- bundler.sh
+++ bundler.sh
@@ -21,7 +21,10 @@ function __loadAsset {
       invoke=''
     fi
     if [ "$type" = "utility" ]; then
-      sh "$__data/includes/gatekeeper.sh" "$name" "$__data/assets/$type/$name/$version/$invoke"  > /dev/null
+      if [[ "$invoke" =~ \.app ]]; then 
+        # Call Gatekeeper for the utility on if '.app' is in the name
+        sh "$__data/includes/gatekeeper.sh" "$name" "$__data/assets/$type/$name/$version/$invoke"  > /dev/null
+      fi
     fi
     echo "$__data/assets/$type/$name/$version/$invoke"
     if [[ ! -z $bundle ]] && [[ $bundle != '..' ]]; then
@@ -29,17 +32,20 @@ function __loadAsset {
     fi
     return 0
   fi
+  # There is no JSON passed to us, so find it in the defaults.
   if [ -z "$json" ]; then
     json="$__data/meta/defaults/$name.json"
   fi
+  # The $json variable should contain either the path to the default or the user-provided path.
   if [ -f "$json" ]; then
+    # Take advantage of the PHP script to install the asset.
     php "$__data/includes/installAsset.php" "$json" "$version"
     if [ ! -z "$result" ]; then
       echo "$result"
       return 0
     fi
     if [ -f "$__data/assets/$type/$name/$version/invoke" ]; then
-      invoke=$(echo `cat "$__data/assets/$type/$name/$version/invoke"`)
+      invoke=`cat "$__data/assets/$type/$name/$version/invoke"`
       if [ "$invoke" = 'null' ]; then
         invoke=''
       fi
@@ -49,7 +55,10 @@ function __loadAsset {
       fi
       if [ $type = 'utility' ]; then
         if [ ! -z "$invoke" ]; then
-          sh "$__data/includes/gatekeeper.sh" "$name" "$__data/assets/$type/$name/$version/$invoke" > /dev/null
+          if [[ "$invoke" =~ \.app ]]; then 
+            # Call Gatekeeper for the utility on if '.app' is in the name
+            sh "$__data/includes/gatekeeper.sh" "$name" "$__data/assets/$type/$name/$version/$invoke" > /dev/null
+          fi
         fi
       fi
       return 0
