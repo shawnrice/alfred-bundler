34666e21c2cf4b58524afbade421f1398f15f7dc
Shawn Rice <rice.shawn@gmail.com>
2014-07-14 12:02:44 -0400

    made variables internal

--- alfred-bundler.rb
+++ alfred-bundler.rb
@@ -10,9 +10,7 @@ require 'json'
 require 'fileutils'
 require 'open-uri'
 
-$ab_major_version = 'aries'
-$ab_data_dir = File.expand_path(
-	"~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-#{$ab_major_version}")
+
 
 ###
 require 'open-uri'
@@ -29,11 +27,18 @@ end
 module Alfred
 
 	class Bundler
+
+		def initialize
+			@major_version = "aries"
+			@data = File.expand_path(
+				"~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-@major_version")
+		end
+
 		# Function to get icons from the icon server
 		def get_icon(font, color, name)
 
 			# Construct the icon directory
-			icon_dir = File.join($ab_data_dir, 'assets/icons', font, color)
+			icon_dir = File.join(@data, 'assets/icons', font, color)
 
 			#  Make the icon directory if it doesn't exist
 			FileUtils.mkpath(icon_dir) unless File.directory?(icon_dir)
@@ -72,34 +77,33 @@ module Alfred
 			end
 			icon_path
 		end
-	end
+	
 
-	def install_bundler()
-	# This is the function to install the bundler
+		def install_bundler()
+			# This is the function to install the bundler
 
-	# Bundler Install URLs
-	# I added a bundler backup at Bitbucket: https://bitbucket.org/shawnrice/alfred-bundler
-	install_urls = [ 'https://github.com/shawnrice/alfred-bundler/tree/aries' ]
-	# https://github.com/shawnrice/alfred-bundler/blob/aries/meta/installer.sh
-	# https://bitbucket.org/shawnrice/alfred-bundler/get/7c0f71f72bfc.zip
-	end
-
-	def _load(name, version, type, json='')
-		unless json.nil?
-			puts "The file does not exist" unless File.exists?(json)
+			# Bundler Install URLs
+			# I added a bundler backup at Bitbucket: https://bitbucket.org/shawnrice/alfred-bundler
+			bundler_urls = IO.readlines("meta/bundler_servers")
 		end
 
-		# This is the function to load an asset
-	end
+		def _load(name, version, type, json='')
+			unless json.nil?
+				puts "The file does not exist" unless File.exists?(json)
+			end
 
-	def _load_asset()
-		# This is done internally
-	end
+			# This is the function to load an asset
+		end
 
-	def _load_asset_inner()
-		# This is done even more internally
-	end
+		def _load_asset()
+			# This is done internally
+		end
 
+		def _load_asset_inner()
+			# This is done even more internally
+		end
+
+	end
 
 end
 
