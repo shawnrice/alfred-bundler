9daa582d3a75abf21c3998df6fd006f2096e3c99
Shawn Rice <rice.shawn@gmail.com>
2014-07-15 16:44:24 -0400

    moved install function back to wrapper

--- bundler/AlfredBundler.rb
+++ bundler/AlfredBundler.rb
@@ -66,49 +66,6 @@ module AlfredBundler
 		icon_path
 	end
 
-	# This is the function to install the bundler
-	def install_bundler()
-		# Make the bundler path
-		FileUtils.mkpath(@data) unless File.directory?(@data)
-
-		# Check for an Internet connection
-		unless server_test("http://www.google.com")
-			abort("ERROR: Cannot install Alfred Bundler because there is no Internet connection.")
-		end
-
-		require 'uri'
-
-		# Bundler Install URLs
-		# I added a bundler backup at Bitbucket: https://bitbucket.org/shawnrice/alfred-bundler
-		# bundler_urls = IO.readlines("meta/bundler_servers")
-		# Bundler URLs have to be hard coded in the wrapper
-		bundler_urls = ["https://github.com/shawnrice/alfred-bundler/archive/devel.zip",
-										"https://bitbucket.org/shawnrice/alfred-bundler/get/devel.zip"]
-		url = bundler_urls.each do |x|
-			server = URI.parse(x)
-			if server_test("#{server.scheme}://#{server.host}")
-				break x
-			end
-		end
-		FileUtils.mkpath(@cache) unless File.directory?(@cache)
-		# Pausing this until we decide to stay with zip or move to git
-
-		# Get the file if it doesn't exist
-		open(@cache + "/bundler.zip", 'wb') do |file|
-			file << open(url).read
-		end
-		zip = unzip("bundler.zip", @cache)
-
-		unless :zip
-			abort("ERROR: Cannot install Alfred Bundler -- bad zip file.")
-		end
-
-		# Theoretically, this will install the bundler
-		command = "bash '" + @cache + "/alfred-bundler-" + @major_version + "/bundler/meta/installer.sh'"
-		success = system(command)
-		success && $?.exitstatus == 0
-	end
-
 	# This is real fucking inelegant, but we can't assume that the
 	# native gems are available to unzip files, so we'll go through the system
 	def unzip(file, destination)
