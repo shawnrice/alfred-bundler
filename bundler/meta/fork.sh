3c38789841a70d04ff690b1632e8b23a48367b1c
Shawn Rice <rice.shawn@gmail.com>
2014-07-18 18:41:40 -0400

    added generic fork script

---
+++ bundler/meta/fork.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# At least a few of the scripting binaries native on OS X are not compiled with
+# the functions to fork processes, so this file serves as a wrapper to fork a
+# process indirectly. Call this script with
+# '/bin/prog' 'fullpathtoscriptname' 'args....'
+
+# Path to base of bundler directory
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd -P )"
+
+for arg in "$@" ; do
+  args="${args} '$arg'"
+done
+
+# Execute the update script entirely in the background
+nohup ${args} 1>&2 &> /dev/null 1>&2 &> /dev/null &
+
+exit
