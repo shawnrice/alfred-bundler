a39b51369dc4d275aa431f3298bbe0cae1ea9cfb
Shawn Patrick Rice <rice@shawnrice.org>
2014-07-29 00:35:45 -0400

    Formatting

--- bundler/AlfredBundler.rb
+++ bundler/AlfredBundler.rb
@@ -14,277 +14,277 @@
 
 module AlfredBundler
 
-	# def initialize
-	# 	@major_version = "devel"
-	# 	@data = File.expand_path(
-	# 		"~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version)
-	# 	@cache = File.expand_path(
-	# 		"~/Library/Caches/com.runningwithcrayons.Alfred-2/Workflow Data/alfred.bundler-" + @major_version)
-	# end
+  # def initialize
+  # 	@major_version = "devel"
+  # 	@data = File.expand_path(
+  # 		"~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version)
+  # 	@cache = File.expand_path(
+  # 		"~/Library/Caches/com.runningwithcrayons.Alfred-2/Workflow Data/alfred.bundler-" + @major_version)
+  # end
 
-	# Checks to see if a server is available
-	def server_test(server)
-		begin
-			true if open(server)
-		rescue
-			false
-		end
-	end
+  # Checks to see if a server is available
+  def server_test(server)
+    begin
+      true if open(server)
+    rescue
+      false
+    end
+  end
 
-	# Function to get icons from the icon server
-	def icon(font, color, name)
+  # Function to get icons from the icon server
+  def icon(font, color, name)
 
-		# Construct the icon directory
-		icon_dir = File.join(@data, 'data/assets/icons', font, color)
+    # Construct the icon directory
+    icon_dir = File.join(@data, 'data/assets/icons', font, color)
 
-		#  Make the icon directory if it doesn't exist
-		FileUtils.mkpath(icon_dir) unless File.directory?(icon_dir)
+    #  Make the icon directory if it doesn't exist
+    FileUtils.mkpath(icon_dir) unless File.directory?(icon_dir)
 
-		# Construct the icon path
-		icon_path = File.join(icon_dir, name + '.png')
+    # Construct the icon path
+    icon_path = File.join(icon_dir, name + '.png')
 
-		# The file exists, so we'll just return the path
-		return icon_path if File.exists?(icon_path)
+    # The file exists, so we'll just return the path
+    return icon_path if File.exists?(icon_path)
 
-		# The file doesn't exist, so we'll have to go through the effort to get it
+    # The file doesn't exist, so we'll have to go through the effort to get it
 
-		# A list of icon servers so that we can have fallbacks
-		icon_servers = IO.readlines("bundler/meta/icon_servers")
-
-
-		# Loop through the list of servers until we find one that is working
-		server = icon_servers.each do |x|
-			if server_test(x)
-				break x
-			end
-		end
-
-		# So, none of the servers were reachable. So, we exit, disgracefully.
-		unless :server
-			return false
-		end
-		# Finish constructing the URL
-		icon_url = "#{server}/icon/#{font}/#{color}/#{name}"
-
-		unless
-			# Get the file if it doesn't exist
-			open(icon_path, 'wb') do |file|
-				file << open(icon_url).read
-			end
-		end
-		icon_path
-	end
-
-	# This is real fucking inelegant, but we can't assume that the
-	# native gems are available to unzip files, so we'll go through the system
-	def unzip(file, destination)
-		command = "cd \"#{destination}\"; unzip -oq #{file}; cd -"
-		success = system(command)
-		success && $?.exitstatus == 0
-	end
-
-	def load_utility(name, version='default', json='')
-		return load(name, version, 'utility', json='')
-	end
-
-	def load_gem(name, version='default', json='')
-		return load(name, version, 'gem', json='')
-	end
-
-	# This is just an idea to load multiple gems at once
-	def load_gems( gems )
-		gems.each do |x|
-			args = x.count
-			if args == 1
-				load_gem(x[0])
-			elsif args == 2
-				load_gem(x[0], x[1])
-			else
-				abort("ERROR: Overloading the load_gems method")
-			end
-		end
-	end
-
-	# This is the function to load an asset
-	def load(name, version, type, json='')
-		unless json.empty?
-			puts "The file does not exist" unless File.exists?(json)
-		end
-
-		# Install the CFPropertyList for us to use if necessary
-		gems = File.expand_path("~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version + "/data/assets/ruby/gems/gems")
-		install_gem("CFPropertyList", "default") unless Dir["#{gems}/CFPropertyList-*"][0]
-		$LOAD_PATH.unshift Dir["#{gems}/CFPropertyList-*/lib"][0]
-		require 'CFPropertyList'
-
-		# Get the bundle id of the workflow
-		if File.exists?('info.plist')
-			plist = CFPropertyList::List.new(:file => "info.plist")
-			data = CFPropertyList.native_types(plist.value)
-			bundle = data["bundleid"]
-		end
-
-		# Let's look in the Bundler's gems directory if the type is a gem
-		if type == "gem"
-			# Gems folder
-
-			# If the version is default, then we'll see if one exists
-			if version == "default"
-				dir = Dir["#{@gem_dir}/gems/#{name}-*/lib"][0]
-			else
-				dir = "#{@gem_dir}/gems/#{name}-#{version}/lib"
-			end
-
-			# This is redundant and stopgap -- find a more elegant way
-			if dir.nil?
-				install_gem(name, version)
-				dir = Dir["#{@gem_dir}/gems/#{name}-*/lib"][0]
-			end
-
-			# We need to put some error checking in here
-			install_gem(name, version) unless File.exists?(dir)
-			$LOAD_PATH.unshift dir
-			require name
-			# This returns TRUE, but we need to get rid of that
-		elsif type == "utility"
-			path = load_asset(name, version, type)
-			return path
-		end
-	end
-
-	# Function to install a gem
-	def install_gem(name, version)
-		# The Bundler Gems path
-		# gems = File.expand_path("~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version + "/data/assets/ruby/gems")
-		FileUtils.mkpath(@gem_dir) unless File.directory?(@gem_dir)
-		if version == "default"
-			command = "gem install -i \"#{@gem_dir}\" #{name}"
-		else
-			command = "gem install -v #{version} -i \"#{@gem_dir}\" #{name}"
-		end
-
-		# We need to doublecheck the security of this one...
-		return `#{command}`
-		# success = system(command)
-		# success && $?.exitstatus == 0
-	end
-
-	# This is done internally
-	def load_asset(name, version, type, bundle='', json='')
-		# Need to add the path caching here
-		if type == "utility"
-			command = "'" + @data + "/bundler/wrappers/alfred.bundler.misc.sh' '#{name}' '#{version}' '#{type}' '#{json}'"
-			return `#{command}`.strip
-		end
-		return false
-	end
+    # A list of icon servers so that we can have fallbacks
+    icon_servers = IO.readlines("bundler/meta/icon_servers")
 
 
-	# This is a bunch of code for a nice little easter egg.
-	def notify(title, message, options)
-		require 'Shellwords'
+    # Loop through the list of servers until we find one that is working
+    server = icon_servers.each do |x|
+      if server_test(x)
+        break x
+      end
+    end
 
-		path = load_utility('Terminal-Notifier')
+    # So, none of the servers were reachable. So, we exit, disgracefully.
+    unless :server
+      return false
+    end
+    # Finish constructing the URL
+    icon_url = "#{server}/icon/#{font}/#{color}/#{name}"
 
-		unless options["sender"].nil?
-			sender = "-sender '" + options["sender"] + "'"
-		else
-			sender = ""
-		end
+    unless
+      # Get the file if it doesn't exist
+      open(icon_path, 'wb') do |file|
+        file << open(icon_url).read
+      end
+    end
+    icon_path
+  end
 
-		unless options["appIcon"].nil?
-			appIcon = "-appIcon '" + options["appIcon"] + "'"
-		else
-			appIcon = ""
-		end
+  # This is real fucking inelegant, but we can't assume that the
+  # native gems are available to unzip files, so we'll go through the system
+  def unzip(file, destination)
+    command = "cd \"#{destination}\"; unzip -oq #{file}; cd -"
+    success = system(command)
+    success && $?.exitstatus == 0
+  end
 
-		unless options["contentImage"].nil?
-			contentImage = "-contentImage '" + options["contentImage"] + "'"
-		else
-			contentImage = ""
-		end
+  def load_utility(name, version='default', json='')
+    return load(name, version, 'utility', json='')
+  end
 
-		unless options["subtitle"].nil?
-			subtitle = "-subtitle '" + options["subtitle"] + "'"
-		else
-			subtitle = ""
-		end
+  def load_gem(name, version='default', json='')
+    return load(name, version, 'gem', json='')
+  end
 
-		unless options["group"].nil?
-			group = "-group '" + options["group"] + "'"
-		else
-			group = ""
-		end
+  # This is just an idea to load multiple gems at once
+  def load_gems( gems )
+    gems.each do |x|
+      args = x.count
+      if args == 1
+        load_gem(x[0])
+      elsif args == 2
+        load_gem(x[0], x[1])
+      else
+        abort("ERROR: Overloading the load_gems method")
+      end
+    end
+  end
 
-		unless options["sound"].nil?
-			sound = "-sound '" + options["sound"] + "'"
-		else
-			sound = ""
-		end
+  # This is the function to load an asset
+  def load(name, version, type, json='')
+    unless json.empty?
+      puts "The file does not exist" unless File.exists?(json)
+    end
 
-		unless options["remove"].nil?
-			remove = "-remove '" + options["remove"] + "'"
-		else
-			remove = ""
-		end
+    # Install the CFPropertyList for us to use if necessary
+    gems = File.expand_path("~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version + "/data/assets/ruby/gems/gems")
+    install_gem("CFPropertyList", "default") unless Dir["#{gems}/CFPropertyList-*"][0]
+    $LOAD_PATH.unshift Dir["#{gems}/CFPropertyList-*/lib"][0]
+    require 'CFPropertyList'
 
-		unless options["list"].nil?
-			list = "-list '" + options["list"] + "'"
-		else
-			list = ""
-		end
+    # Get the bundle id of the workflow
+    if File.exists?('info.plist')
+      plist = CFPropertyList::List.new(:file => "info.plist")
+      data = CFPropertyList.native_types(plist.value)
+      bundle = data["bundleid"]
+    end
 
-		unless options["activate"].nil?
-			activate = "-activate '" + options["activate"] + "'"
-		else
-			activate = ""
-		end
+    # Let's look in the Bundler's gems directory if the type is a gem
+    if type == "gem"
+      # Gems folder
 
-		unless options["open"].nil?
-			openURL = "-open '" + options["open"] + "'"
-		else
-			openURL = ""
-		end
+      # If the version is default, then we'll see if one exists
+      if version == "default"
+        dir = Dir["#{@gem_dir}/gems/#{name}-*/lib"][0]
+      else
+        dir = "#{@gem_dir}/gems/#{name}-#{version}/lib"
+      end
 
-		unless options["execute"].nil?
-			execute = "-execute '" + options["execute"] + "'"
-		else
-			execute = ""
-		end
+      # This is redundant and stopgap -- find a more elegant way
+      if dir.nil?
+        install_gem(name, version)
+        dir = Dir["#{@gem_dir}/gems/#{name}-*/lib"][0]
+      end
 
-		# unless icon.empty?
-		# 	if (File.exists?(icon) && File.extname(icon) == '.icns')
-		# 		icon_moved = true
-		# 		tmp_icon_path = @cache + '/tmp-icon'
-		# 		FileUtils.mkpath(tmp_icon_path) unless File.directory?(tmp_icon_path)
-		# 		tn_icon = File.path(File.dirname(path) + '/../Resources/Terminal.icns')
-		# 		tn_icon_path = File.dirname(tn_icon)
-		#
-		# 		puts tn_icon_path + '/' + File.basename(icon)
-		#
-		#
-		# 		FileUtils.move tn_icon, tmp_icon_path
-		#
-		# 		FileUtils.copy icon, tn_icon_path
-		# 		FileUtils.move tn_icon_path + '/' + File.basename(icon), tn_icon_path + '/Terminal.icns'
-		# 		abort
-		# 	end
-		# end
-		# icon = '/Users/Sven/Documents/Alfred2 Workflows/alfred-bundler/bundler/meta/icons/bundle.png'
-		# command = "#{Shellwords.escape(path)} -t '#{title}' -m '#{message}'"
-		# -contentImage '/Users/Sven/Desktop/mics.png'
-		# com.runningwithcrayons.Alfred-2
-		command = "#{Shellwords.escape(path)} -title '#{title}' -message '#{message}' "
-		command = "#{command} #{execute} #{openURL} #{activate} #{remove} #{sound} "
-		command = "#{command} #{list} #{subtitle} #{contentImage} #{appIcon} #{sender}"
+      # We need to put some error checking in here
+      install_gem(name, version) unless File.exists?(dir)
+      $LOAD_PATH.unshift dir
+      require name
+      # This returns TRUE, but we need to get rid of that
+    elsif type == "utility"
+      path = load_asset(name, version, type)
+      return path
+    end
+  end
 
-		system(command)
+  # Function to install a gem
+  def install_gem(name, version)
+    # The Bundler Gems path
+    # gems = File.expand_path("~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version + "/data/assets/ruby/gems")
+    FileUtils.mkpath(@gem_dir) unless File.directory?(@gem_dir)
+    if version == "default"
+      command = "gem install -i \"#{@gem_dir}\" #{name}"
+    else
+      command = "gem install -v #{version} -i \"#{@gem_dir}\" #{name}"
+    end
 
-		# if :icon_moved
-		# 	FileUtils.remove tn_icon
-		# 	FileUtils.copy tmp_icon_path, tn_icon_path
-		# end
-	end
+    # We need to doublecheck the security of this one...
+    return `#{command}`
+    # success = system(command)
+    # success && $?.exitstatus == 0
+  end
 
-	private :install_gem, :load_asset, :server_test
+  # This is done internally
+  def load_asset(name, version, type, bundle='', json='')
+    # Need to add the path caching here
+    if type == "utility"
+      command = "'" + @data + "/bundler/wrappers/alfred.bundler.misc.sh' '#{name}' '#{version}' '#{type}' '#{json}'"
+      return `#{command}`.strip
+    end
+    return false
+  end
+
+
+  # This is a bunch of code for a nice little easter egg.
+  def notify(title, message, options)
+    require 'Shellwords'
+
+    path = load_utility('Terminal-Notifier')
+
+    unless options["sender"].nil?
+      sender = "-sender '" + options["sender"] + "'"
+    else
+      sender = ""
+    end
+
+    unless options["appIcon"].nil?
+      appIcon = "-appIcon '" + options["appIcon"] + "'"
+    else
+      appIcon = ""
+    end
+
+    unless options["contentImage"].nil?
+      contentImage = "-contentImage '" + options["contentImage"] + "'"
+    else
+      contentImage = ""
+    end
+
+    unless options["subtitle"].nil?
+      subtitle = "-subtitle '" + options["subtitle"] + "'"
+    else
+      subtitle = ""
+    end
+
+    unless options["group"].nil?
+      group = "-group '" + options["group"] + "'"
+    else
+      group = ""
+    end
+
+    unless options["sound"].nil?
+      sound = "-sound '" + options["sound"] + "'"
+    else
+      sound = ""
+    end
+
+    unless options["remove"].nil?
+      remove = "-remove '" + options["remove"] + "'"
+    else
+      remove = ""
+    end
+
+    unless options["list"].nil?
+      list = "-list '" + options["list"] + "'"
+    else
+      list = ""
+    end
+
+    unless options["activate"].nil?
+      activate = "-activate '" + options["activate"] + "'"
+    else
+      activate = ""
+    end
+
+    unless options["open"].nil?
+      openURL = "-open '" + options["open"] + "'"
+    else
+      openURL = ""
+    end
+
+    unless options["execute"].nil?
+      execute = "-execute '" + options["execute"] + "'"
+    else
+      execute = ""
+    end
+
+    # unless icon.empty?
+    # 	if (File.exists?(icon) && File.extname(icon) == '.icns')
+    # 		icon_moved = true
+    # 		tmp_icon_path = @cache + '/tmp-icon'
+    # 		FileUtils.mkpath(tmp_icon_path) unless File.directory?(tmp_icon_path)
+    # 		tn_icon = File.path(File.dirname(path) + '/../Resources/Terminal.icns')
+    # 		tn_icon_path = File.dirname(tn_icon)
+    #
+    # 		puts tn_icon_path + '/' + File.basename(icon)
+    #
+    #
+    # 		FileUtils.move tn_icon, tmp_icon_path
+    #
+    # 		FileUtils.copy icon, tn_icon_path
+    # 		FileUtils.move tn_icon_path + '/' + File.basename(icon), tn_icon_path + '/Terminal.icns'
+    # 		abort
+    # 	end
+    # end
+    # icon = '/Users/Sven/Documents/Alfred2 Workflows/alfred-bundler/bundler/meta/icons/bundle.png'
+    # command = "#{Shellwords.escape(path)} -t '#{title}' -m '#{message}'"
+    # -contentImage '/Users/Sven/Desktop/mics.png'
+    # com.runningwithcrayons.Alfred-2
+    command = "#{Shellwords.escape(path)} -title '#{title}' -message '#{message}' "
+    command = "#{command} #{execute} #{openURL} #{activate} #{remove} #{sound} "
+    command = "#{command} #{list} #{subtitle} #{contentImage} #{appIcon} #{sender}"
+
+    system(command)
+
+    # if :icon_moved
+    # 	FileUtils.remove tn_icon
+    # 	FileUtils.copy tmp_icon_path, tn_icon_path
+    # end
+  end
+
+  private :install_gem, :load_asset, :server_test
 end
--- bundler/wrappers/alfred.bundler.rb
+++ bundler/wrappers/alfred.bundler.rb
@@ -1,8 +1,5 @@
 #!/bin/ruby
 
-
-
-
 # This is very experimental code written by some people who don't really know ruby well.
 # Watch it develop.
 
@@ -18,116 +15,116 @@ require 'AlfredBundler'
 
 module Alfred
 
-	class Bundler
-		include AlfredBundler
-		# @major_version = "devel"
-		# @data = File.expand_path(
-		# 	"~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version)
-		# @cache = File.expand_path(
-		# 	"~/Library/Caches/com.runningwithcrayons.Alfred-2/Workflow Data/alfred.bundler-" + @major_version)
-		def initialize
-			@major_version = "devel"
-			@home = File.expand_path("~/")
-			@data = @home + "/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version
-			@cache = @home + "/Library/Caches/com.runningwithcrayons.Alfred-2/Workflow Data/alfred.bundler-" + @major_version
+  class Bundler
+    include AlfredBundler
+    # @major_version = "devel"
+    # @data = File.expand_path(
+    # 	"~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version)
+    # @cache = File.expand_path(
+    # 	"~/Library/Caches/com.runningwithcrayons.Alfred-2/Workflow Data/alfred.bundler-" + @major_version)
+    def initialize
+      @major_version = "devel"
+      @home = File.expand_path("~/")
+      @data = @home + "/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-" + @major_version
+      @cache = @home + "/Library/Caches/com.runningwithcrayons.Alfred-2/Workflow Data/alfred.bundler-" + @major_version
 
-			# Add our local gem repository
-			@gem_dir = @data + "/data/assets/ruby/gems"
-			$LOAD_PATH.unshift @gem_dir
-			# The below line is just for easier development purposes
-			#
-			# Should there be a better test?
-			install_bundler	unless File.exists?(@data + "/bundler/AlfredBundler.rb")
+      # Add our local gem repository
+      @gem_dir = @data + "/data/assets/ruby/gems"
+      $LOAD_PATH.unshift @gem_dir
+      # The below line is just for easier development purposes
+      #
+      # Should there be a better test?
+      install_bundler	unless File.exists?(@data + "/bundler/AlfredBundler.rb")
 
-			# $LOAD_PATH.unshift @data + "/bundler"
-			# $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
-			# require 'AlfredBundler'
+      # $LOAD_PATH.unshift @data + "/bundler"
+      # $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
+      # require 'AlfredBundler'
 
-		end
+    end
 
 
-		# This is the function to install the bundler
-		def install_bundler()
-			# Make the bundler path
-			FileUtils.mkpath(@data) unless File.directory?(@data)
+    # This is the function to install the bundler
+    def install_bundler()
+      # Make the bundler path
+      FileUtils.mkpath(@data) unless File.directory?(@data)
 
-			# Check for an Internet connection
-			unless server_test("http://www.google.com")
-				abort("ERROR: Cannot install Alfred Bundler because there is no Internet connection.")
-			end
+      # Check for an Internet connection
+      unless server_test("http://www.google.com")
+        abort("ERROR: Cannot install Alfred Bundler because there is no Internet connection.")
+      end
 
-			require 'uri'
+      require 'uri'
 
-			# Bundler Install URLs
-			# I added a bundler backup at Bitbucket: https://bitbucket.org/shawnrice/alfred-bundler
-			# bundler_urls = IO.readlines("meta/bundler_servers")
-			# Bundler URLs have to be hard coded in the wrapper
-			bundler_urls = ["https://github.com/shawnrice/alfred-bundler/archive/" + @major_version + "-latest.zip",
-											"https://bitbucket.org/shawnrice/alfred-bundler/get/" + @major_version + "-latest.zip"]
-			url = bundler_urls.each do |x|
-				server = URI.parse(x)
-				if server_test("#{server.scheme}://#{server.host}")
-					break x
-				end
-			end
-			FileUtils.mkpath(@cache) unless File.directory?(@cache)
-			# Pausing this until we decide to stay with zip or move to git
+      # Bundler Install URLs
+      # I added a bundler backup at Bitbucket: https://bitbucket.org/shawnrice/alfred-bundler
+      # bundler_urls = IO.readlines("meta/bundler_servers")
+      # Bundler URLs have to be hard coded in the wrapper
+      bundler_urls = ["https://github.com/shawnrice/alfred-bundler/archive/" + @major_version + "-latest.zip",
+                      "https://bitbucket.org/shawnrice/alfred-bundler/get/" + @major_version + "-latest.zip"]
+      url = bundler_urls.each do |x|
+        server = URI.parse(x)
+        if server_test("#{server.scheme}://#{server.host}")
+          break x
+        end
+      end
+      FileUtils.mkpath(@cache) unless File.directory?(@cache)
+      # Pausing this until we decide to stay with zip or move to git
 
-			# Get the file if it doesn't exist
-			open(@cache + "/bundler.zip", 'wb') do |file|
-				file << open(url).read
-			end
-			zip = unzip("bundler.zip", @cache)
+      # Get the file if it doesn't exist
+      open(@cache + "/bundler.zip", 'wb') do |file|
+        file << open(url).read
+      end
+      zip = unzip("bundler.zip", @cache)
 
-			unless :zip
-				abort("ERROR: Cannot install Alfred Bundler -- bad zip file.")
-			end
+      unless :zip
+        abort("ERROR: Cannot install Alfred Bundler -- bad zip file.")
+      end
 
-			# Theoretically, this will install the bundler
-			command = "bash '" + @cache + "/alfred-bundler-" + @major_version + "/bundler/meta/installer.sh'"
-			success = system(command)
-			success && $?.exitstatus == 0
-		end
+      # Theoretically, this will install the bundler
+      command = "bash '" + @cache + "/alfred-bundler-" + @major_version + "/bundler/meta/installer.sh'"
+      success = system(command)
+      success && $?.exitstatus == 0
+    end
 
-	end
+  end
 
 end
 
 
 if __FILE__ == $0
-	name  = 'align-center'
-	color = '2321ee'
-	font  = 'fontawesome'
+  name  = 'align-center'
+  color = '2321ee'
+  font  = 'fontawesome'
 
-	bundler = Alfred::Bundler.new
-
-
-	### TEST CASES
-	########################
+  bundler = Alfred::Bundler.new
 
 
-	# PASSING
+  ### TEST CASES
+  ########################
 
-	# puts bundler.load('Pashua', 'default', 'utility')
-	# puts bundler.load_utility('Pashua', 'default')
-	# bundler.load_gem('zip', '2.0.2')
-	icon = File.join( File.expand_path(File.dirname(__FILE__)), '..', 'meta', 'icons', 'bundle.icns')
 
-	# For some reason, this doesn't work when there are spaces in the paths — either
-	# quoted or escaped
-	bundler_icon = File.expand_path("~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-devel/bundler/meta/icons/bundle.icns")
-	puts bundler_icon
-	options = {
-		"sender" => "com.runningwithcrayons.Alfred-2",
-		"subtitle" => "This is a subtitle",
-		"appIcon" => bundler_icon
-	}
+  # PASSING
 
-	bundler.notify('Title','Message', options)
-	puts bundler.icon(font, color, name)
+  # puts bundler.load('Pashua', 'default', 'utility')
+  # puts bundler.load_utility('Pashua', 'default')
+  # bundler.load_gem('zip', '2.0.2')
+  icon = File.join( File.expand_path(File.dirname(__FILE__)), '..', 'meta', 'icons', 'bundle.icns')
 
-	# FAILING
+  # For some reason, this doesn't work when there are spaces in the paths — either
+  # quoted or escaped
+  bundler_icon = File.expand_path("~/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-devel/bundler/meta/icons/bundle.icns")
+  puts bundler_icon
+  options = {
+    "sender" => "com.runningwithcrayons.Alfred-2",
+    "subtitle" => "This is a subtitle",
+    "appIcon" => bundler_icon
+  }
 
-	# bundler.load_gem('rspec')
-	# bundler.load_gems([['zip'], ['test', 2]]) # This will fail because there is no test gem v 2
+  bundler.notify('Title','Message', options)
+  puts bundler.icon(font, color, name)
+
+  # FAILING
+
+  # bundler.load_gem('rspec')
+  # bundler.load_gems([['zip'], ['test', 2]]) # This will fail because there is no test gem v 2
 end
