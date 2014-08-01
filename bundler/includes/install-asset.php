4909976915c646623b9c5eaa3e370e82c2fdc9f2
Shawn Patrick Rice <rice@shawnrice.org>
2014-08-01 02:26:05 -0400

    Removed testing message. Sent error to stderr. Added correct exit codes

--- bundler/includes/install-asset.php
+++ bundler/includes/install-asset.php
@@ -6,18 +6,16 @@
  * 
  */
 
-
 require_once( __DIR__ . '/../AlfredBundler.php' );
 
 $b = new AlfredBundlerInternalClass;
-
-print_r( $argv );
 
 if ( isset( $argv[1] ) ) {
   if ( file_exists( $argv[1] ) ) {
     $json = $argv[1];
   } else {
-    echo "Error: json path not found ('{$argv[1]}')";
+    file_put_contents( 'php://stderr', "Error: json path not found ('{$argv[1]}')" );
+    exit( 1 );
   }
 }
 
