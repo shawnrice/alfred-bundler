f07a540122db4a0cf23da8345a8ce6d6cd6f266b
deanishe <deanishe@deanishe.net>
2014-08-06 22:16:29 +0200

    Change cache name to `module-function.cache`

--- bundler/AlfredBundler.py
+++ bundler/AlfredBundler.py
@@ -327,8 +327,8 @@ class cached(object):
 
     def __init__(self, func):
         self.func = func
-        self.cachepath = os.path.join(HELPER_DIR, '{}.cache'.format(
-                                      func.__name__))
+        self.cachepath = os.path.join(HELPER_DIR, '{}-{}.cache'.format(
+                                      __name__, func.__name__))
         # self.func = func
         self.cache = {}
 
