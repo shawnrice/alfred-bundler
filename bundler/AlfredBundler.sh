aa76937f87102074af96fb3d8e266df15dc86565
deanishe <deanishe@deanishe.net>
2014-08-06 16:48:11 +0200

    Use branch from env

--- bundler/AlfredBundler.sh
+++ bundler/AlfredBundler.sh
@@ -17,7 +17,12 @@
 # Path to base of bundler directory
 declare -r AB_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
   # Get the major version from the file
-declare -r AB_MAJOR_VERSION=$(cat "${AB_PATH}/meta/version_major")
+if [ ! -z "${ALFRED_BUNDLER_DEVEL}" ]; then
+  declare -r AB_MAJOR_VERSION="${ALFRED_BUNDLER_DEVEL}"
+else
+  declare -r AB_MAJOR_VERSION=$(cat "${AB_PATH}/meta/version_major")
+fi
+
 # Set the data directory
 declare -r AB_DATA="${HOME}/Library/Application Support/Alfred 2/Workflow Data/alfred.bundler-${AB_MAJOR_VERSION}"
 
