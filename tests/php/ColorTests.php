8e3067fa6891b54f6a040c92a3bb6b426111ae7d
Shawn Rice <rice.shawn@gmail.com>
2014-08-09 16:07:44 -0400

    started color test file for php icon conversion

--- tests/php/ColorTests.php
+++ tests/php/ColorTests.php
@@ -2,5 +2,11 @@
 
 
 require_once( __DIR__ . '/../../bundler/bundlets/alfred.bundler.php' );
+
+$_ENV['AB_BRANCH'] = 'devel';
+$_ENV[''] = '';
+$_ENV[''] = '';
+$_ENV[''] = '';
+
 $b = new AlfredBundler;
 $i = new AlfredBundlerIcon;
