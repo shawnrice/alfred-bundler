d83bc05e923395ca40aec7c59a5e8357b38d3211
Shawn Rice <rice.shawn@gmail.com>
2014-08-14 20:31:10 -0400

    testing notes

--- tests/php/AlfredBundler.php
+++ tests/php/AlfredBundler.php
@@ -7,17 +7,32 @@
  */
 
 
-require_once( __DIR__ . '/../../bundler/wrappers/alfred.bundler.php' );
+// tests to use info.plist
+// tests to use env variables
 
 
 class AlfredBundlerTest extends PHPUnit_Framework_TestCase
 {
-    public function setUp(){ }
-    public function tearDown(){ }
+    public function setUp(){
+        require_once( __DIR__ . '/../../bundler/wrappers/alfred.bundler.php' );
+
+        // define data and cache paths first...
+        if ( file_exists( DATADIR ) )
+            rmdir( DATADIR );
+        if ( file_exists( CACHEDIR ) )
+            rmdir( CACHEDIR ); // make these work recursively
+        }
+
+        $this->b = new AlfredBundler();
+    }
+
+    public function tearDown(){
+        unset( $this->b )
+    }
 
     public function testLoadLibrary() {
-      $b = new AlfredBundler();
-      $this->assertTrue( $b->library( 'Workflows' ) );
+
+      $this->assertTrue( $this->b->library( 'Workflows' ) );
     }
 
 }
