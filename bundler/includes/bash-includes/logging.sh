e4a6e1a0ecfa6d72be77a34a682002daa4bb29af
Shawn Rice <rice.shawn@gmail.com>
2014-08-16 15:26:44 -0400

    refactored logging functions

--- bundler/includes/bash-includes/logging.sh
+++ bundler/includes/bash-includes/logging.sh
@@ -1,148 +1,214 @@
+# Functions in this should be prepended with AB::Log::
+#
 ################################################################################
 ### Start Log Functions
 ################################################################################
 
 # User facing log function
-function AlfredBundler::log() {
-a=0
-}
+# Currently, this is a duplicate of the internal one... but I need to
+# figure out a way to combine them that doesn't hurt the call syntax much...
+function AlfredBundler::Log() {
 
-# Function that outputs information to the terminal (stderr)
-function AlfredBundler::report() {
-# Message — $1
-# Level   — $2
-
-local message
-local level
-local file
-local line
-local internal
-
-local levels
-
-local date
-
-message="$1"
-level="$2"
-
-level=$(echo "${level}" | tr [[:lower:]] [[:upper:]])
-
-[[ "${level}" == "WARN" ]] && level="WARNING"
-[[ "${level}" == "FATAL" ]] && level="CRITICAL"
-
-# @TODO : Add in better error checking for empty arguments
-internal=$(caller)
-line=$(echo "${internal}" | cut -d ' ' -f1)
-file="${internal#${line}}"
-file=$(basename "${file}")
-
-# Define the log levels
-levels=(DEBUG INFO WARNING ERROR CRITICAL)
-
-date="["$(date +"%T")"]"
-
-if [[ $# -lt 2 ]]; then
-  echo "${date} [${file}:${line}] [DEBUG] The logging function needs to be used with a log level and message." >&2
-  return 0
-fi
-
-# If the level is an int, then echo this:
-if [[ $level =~ ^-?[0-9]+$ ]]; then
-  if [ ! -z ${levels[level]} ]; then
-    echo "${date} [${file}:${line}] [${levels[level]}] ${message}" >&2
-  else
-    echo "${date} [${file}:${line}] [WARNING] Invalid log level (${level}); defaulting to 'DEBUG'" >&2
-    echo "${date} [${file}:${line}] [DEBUG] ${message}" >&2
-  fi
- return 0
-elif [[ "${levels[@]}" =~ $level ]]; then
-  echo "${date} [${file}:${line}] [${level}] ${message}" >&2
-  return 0
-else
-  echo "${date} [${file}:${line}] [WARNING] Invalid log level (${level}); defaulting to 'DEBUG'" >&2
-  echo "${date} [${file}:${line}] [DEBUG] ${message}" >&2
-fi
-
-
-}
-
-# Internal logging function
-function AlfredBundler::internalLog() {
   local message
   local level
   local file
   local line
   local internal
-
-  local log_path
-  local log
-
   local date
 
-  log="$1"
-  message="$2"
-  level="$3"
+  # Set necessary variables
+  date=$(date +%H:%M:%S)
+  internal=$(caller)
+  line=$(echo "${internal}" | cut -d ' ' -f1)
+  file="${internal#${line}}"
+  file=$(basename "${file}")
 
-  log_path="${AB_DATA}/data/logs"
-  if [[ ! -d "${log_path}" ]]; then
-    mkdir -m 0775 -p "${log_path}"
+  # Check to see if we have the right number of arguments
+  if [[ $# -lt 2 ]]; then
+    echo "[${date}] [${file}:${line}] [DEBUG] The logging function needs to " \
+        "be used with a log level and message." >&2
+    return 0
+  fi
+
+   # Set the arguments
+  message="$1"
+
+  level=$(AB::Log::normalize_log_level "$2")
+  # Check for valid log level
+  if [[ $? -eq 1 ]]; then
+    echo "[${date}] [${file}:${line}] [WARNING] Log level '$2' is not valid. " \
+         "Falling back to 'INFO' (1)" >&2
+  fi
+
+  [[ -z "${destination}" ]] && destination='console'
+  destination=$(AB::Log::normalize_destination $3)
+  # Check for valid destination
+  if [[ $? -eq 1 ]]; then
+    echo "[${date}] [${file}:${line}] [WARNING] Destination '$2' is not " \
+         "valid. Falling back to 'console'" >&2
+  fi
+
+  message="[${file}:${line}] [${level}] ${message}"
+
+  if [[ "${destination}" == 'console' ]] || [[ "${destination}" == 'both' ]]; then
+    AB::Log::Console "${message}"
+  fi
+  if [[ "${destination}" == 'file' ]] || [[ "${destination}" == 'both' ]]; then
+    AB::Log::File "${message}" "${WF_LOG}"
+  fi
+
+}
+
+function AB::Log::Log() {
+  local message
+  local level
+  local file
+  local line
+  local internal
+  local date
+
+  # Set necessary variables
+  date=$(date +%H:%M:%S)
+  internal=$(caller)
+  line=$(echo "${internal}" | cut -d ' ' -f1)
+  file="${internal#${line}}"
+  file=${file## } # remove whitespace from the front
+  file=$(basename "${file}")
+
+  # Check to see if we have the right number of arguments
+  if [[ $# -lt 2 ]]; then
+    echo "[${date}] [${file}:${line}] [DEBUG] The logging function needs to " \
+        "be used with a log level and message." >&2
+    return 0
+  fi
+
+  # Set the arguments
+  message="$1"
+
+  level=$(AB::Log::normalize_log_level "$2")
+  # Check for valid log level
+  if [[ $? -eq 1 ]]; then
+    echo "[${date}] [${file}:${line}] [WARNING] Log level '$2' is not valid. " \
+         "Falling back to 'INFO' (1)" >&2
+  fi
+
+  [[ -z "${destination}" ]] && destination='console'
+  destination=$(AB::Log::normalize_destination $3)
+  # Check for valid destination
+  if [[ $? -eq 1 ]]; then
+    echo "[${date}] [${file}:${line}] [WARNING] Destination '$2' is not " \
+         "valid. Falling back to 'console'" >&2
+  fi
+
+  message="[${file}:${line}] [${level}] ${message}"
+
+  if [[ "${destination}" == 'console' ]] || [[ "${destination}" == 'both' ]]; then
+    AB::Log::Console "${message}"
+  fi
+  if [[ "${destination}" == 'file' ]] || [[ "${destination}" == 'both' ]]; then
+    AB::Log::File "${message}" "${AB_LOG}"
+  fi
+}
+
+
+
+
+
+# Function that outputs information to the terminal (STDERR)
+function AB::Log::Console() {
+  # Message — $1
+  local date=$(date +%H:%M:%S)
+  echo "[${date}] $1" >&2
+  return 0
+}
+
+function AB::Log::File() {
+  local date=$(date +"%Y-%m-%d %H:%M:%S")
+  local message="$1"
+  local log="$2"
+
+  if [[ ! -d $(dirname "${log}") ]]; then
+    AB::Log::Log "Log directory '${log}' does not exist" ERROR console
+    return 1
+  fi
+
+  echo "[${date}] ${message}" >> "${log}"
+  return 0
+}
+
+# Make log directories
+function AB::Log::Setup() {
+  # This is internal setup...
+  if [[ ! -e "${AB_LOG}" ]]; then
+    mkdir -m 0775 -p "${AB_LOG}"
     if [[ $? -eq 0 ]]; then
-      AlfredBundler::report "Created directory '${log_path}" INFO
+      AB::Log::Log "Created directory '${AB_LOG}'" INFO console
+      return 0
     else
-      AlfredBundler::report "Could not create directory '${log_path}" ERROR
+      AB::Log::Log "Could not create directory '${AB_LOG}'" ERROR console
       return 1
     fi
   fi
+}
 
-  level=$(echo "${level}" | tr [[:lower:]] [[:upper:]])
+function AB::Log::normalize_destination() {
+  local destination=$(echo "$1" | tr [[:upper:]] [[:lower:]])
+  local destinations=(file console both)
+  [[ "${destinations[@]}" =~ $destination ]] && echo $destination && return 0
+  # Log an error here and set the destination to console
+  echo 'console'
+  return 1
+}
+
+
+
+function AB::Log::normalize_log_level() {
+  local levels
+  local level
+
+  # Define the log levels
+  levels=(DEBUG INFO WARNING ERROR CRITICAL)
+  level=$(echo "$1" | tr [[:lower:]] [[:upper:]])
 
   [[ "${level}" == "WARN" ]] && level="WARNING"
   [[ "${level}" == "FATAL" ]] && level="CRITICAL"
 
-  # @TODO : Add in better error checking for empty arguments
-  internal=$(caller)
-  line=$(echo "${internal}" | cut -d ' ' -f1)
-  file="${internal#${line} }"
-
-  # Define the log levels
-  levels=(DEBUG INFO WARNING ERROR CRITICAL)
-
-  date=$(date +"%T")
-
   # If the level is an int, then echo this:
   if [[ $level =~ ^-?[0-9]+$ ]]; then
     if [ ! -z ${levels[level]} ]; then
-      out="[${date}] [${file}:${line}] [${levels[level]}] ${message}"
+      echo ${levels[level]}
+      return 0
     else
-      echo "[${date}] [${file}:${line}] [WARNING] Invalid log level (${level}); defaulting to 'DEBUG'" >&2
-      out="[${date}] [${file}:${line}] [DEBUG] ${message}"
+      echo 'INFO'
+      return 1
     fi
   elif [[ "${levels[@]}" =~ $level ]]; then
-    out="[${date}] [${file}:${line}] [${level}] ${message}"
+    echo $level
+    return 0
   else
-    echo "$[{date}] [${file}:${line}] [WARNING] Invalid log level (${level}); defaulting to 'DEBUG'" >&2
-    out="$[{date}] [${file}:${line}] [DEBUG] ${message}"
+    echo 'INFO'
+    return 1
   fi
+}
 
-  # need to add in error reporting and taming log size
-  [[ ! -f "${log_path}/${log}.log" ]]; touch "${log_path}/${log}.log"
-  AlfredBundler::prepend "${out}" "${log_path}/${log}.log"
+
+function AB::Log::Rotate() {
+  local name
+  local dir
+
+  [[ ! -f "$1" ]] && return 0 # There is no log file to rotate
+  if [[ $(stat -f "%z" "$1") -gt 1048576 ]]; then
+    name=$(basename "$1")
+    name=$(name%\.log)
+    dir=$(dirname "$1")
+    mv -f "$1" "${dir}/${name}1.log"
+    AB::Log::Log "Rotated log '${name}.log" INFO console
+  fi
+  touch "$1"
 
   return 0
-
 }
 
-# Prepends a string to a file
-function AlfredBundler::prepend() {
-  # $1 line
-  # $2 file
-  exec 3<> "${file}" && awk -v TEXT="${line}" 'BEGIN {print TEXT}{print}' "${file}" >&3
-  return 0
-}
-
-function AlfredBundler::rotate_log() {
-  local log
-}
 
 ################################################################################
 ### End Log Functions
