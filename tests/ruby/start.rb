a0cf176eea07671920f306edfa595d95d3e6346e
Shawn Rice <rice.shawn@gmail.com>
2014-08-19 19:14:13 -0400

    info.plist for tests; start.rb (pseudo-testing version) includes utility and load for multiple arg types

--- tests/ruby/start.rb
+++ tests/ruby/start.rb
@@ -3,16 +3,25 @@
 require_relative "../../bundler/bundlets/alfred.bundler.rb"
 
 bundler = Alfred::Bundler.new
-bundler.foo
-puts bundler.icon('font' => 'ELUSive', 'name' => 'fire', 'color' => 'abcabc', 'alter' => true)
 
-puts bundler.icon('elusive', 'fire')
-puts bundler.icon('elusive', 'fire', 'asjdhahs')
-puts bundler.icon('system', 'Accounts')
-puts bundler.icon('system', 'asda')
+puts bundler.load( 'utility', 'Pashua' )
+puts bundler.load( 'php', 'Workflows' )
+puts bundler.utility({'name' => 'CocoaDialog', 'version' => '3.0.0-beta7'})
+puts bundler.utility(['Pashua', 'latest'])
+puts bundler.utility('Pashua', 'latest')
+puts bundler.utility('cocoaDialog')
+puts bundler.utility('viewer')
 
-bundler.console("Test", 'inaasfo')
-bundler.console("Fatal test", 'CRITICAL')
-bundler.log('Testing')
-bundler.log('Testing', 'FATAL')
-puts "hello"
+# bundler.foo
+# puts bundler.icon('font' => 'ELUSive', 'name' => 'fire', 'color' => 'abcabc', 'alter' => true)
+
+# puts bundler.icon('elusive', 'fire')
+# puts bundler.icon('elusive', 'fire', 'asjdhahs')
+# puts bundler.icon('system', 'Accounts')
+# puts bundler.icon('system', 'asda')
+
+# bundler.console("Test", 'inaasfo')
+# bundler.console("Fatal test", 'CRITICAL')
+# bundler.log('Testing')
+# bundler.log('Testing', 'FATAL')
+#
