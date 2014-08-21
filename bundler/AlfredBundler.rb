2aa63679a3abf986d3442346324c763d66eb0baf
Shawn Rice <rice.shawn@gmail.com>
2014-08-21 03:09:27 -0400

    some refactoring initial documentation structure

--- bundler/AlfredBundler.rb
+++ bundler/AlfredBundler.rb
@@ -1,7 +1,27 @@
+
+#
+# [module description]
+#
+# @author [Sven]
+# @since Taurus 1
+#
 module Alfred
 
+
+  #
+  # [class description]
+  #
+  # @author [Sven]
+  #
   class Internal
 
+
+    #
+    # [initialize description]
+    # @param data [type] [description]
+    # @param cache [type] [description]
+    #
+    # @return [type] [description]
     def initialize(data, cache)
       @data   = data
       @cache  = cache
@@ -23,19 +43,33 @@ module Alfred
       # Add our local gem repository
       @gem_dir = @data + "/data/assets/ruby/gems"
       Gem.path.unshift(@gem_dir) unless Gem.path.include?(@gem_dir)
-      # install_gem("CFPropertyList", "default") unless Dir["#{gems}/CFPropertyList-*"][0]
-      # require 'CFPropertyList'
-
     end
 
+
+    #
+    # [plist_check description]
+    #
+    # @return [type] [description]
     def plist_check
       self.console('No plist exists.', 'fatal') unless File.exists? './info.plist'
     end
 
+
+    #
+    # [method_missing description]
+    # @param name [type] [description]
+    # @param *arguments [type] [description]
+    #
+    # @return [type] [description]
     def method_missing(name, *arguments)
       self.console("'#{name}' does not exist either externally or internally.", 'error')
     end
 
+
+    #
+    # [initialize_modern description]
+    #
+    # @return [type] [description]
     def initialize_modern
       # For Alfred >= v2.4:277
       @background = ENV['alfred_theme_background']
@@ -44,13 +78,25 @@ module Alfred
       @name       = ENV['alfred_workflow_name']
     end
 
+
+    #
+    # [initialize_deprecated description]
+    #
+    # @return [type] [description]
     def initialize_deprecated
       # For Alfred < v2.4:277
       @name   = read_plist_value('name', 'info.plist')
       @bundle = read_plist_value('bundleid', 'info.plist')
     end
 
-    # We could do this with a Gem... but let's just not.
+
+
+    #
+    # [read_plist_value description]
+    # @param key [type] [description]
+    # @param plist [type] [description]
+    #
+    # @return [type] [description]
     def read_plist_value(key, plist)
       return `/usr/libexec/PlistBuddy -c 'Print :#{key}' '#{plist}'`
     end
@@ -59,6 +105,12 @@ module Alfred
     ######################
     #### LOAD FUNCTIONS
 
+
+    #
+    # [load description]
+    # @param *args [type] [description]
+    #
+    # @return [type] [description]
     def load(*args)
       args = parse_load_args(args)
       type = args['type']
@@ -83,16 +135,28 @@ module Alfred
 
     end
 
+
+    #
+    # [parse_load_args description]
+    #
+    # @overload parse_load_args(args)
+    #   @param hash
+    #
+    # @overload parse_load_args(args)
+    #   @param array
+    #
+    # @overload parse_load_args(args)
+    #   @param
+    #   @param
+    #   @param
+    #
+    # @param *args [type] [description]
+    #
+    # @return [type] [description]
     def parse_load_args(*args)
       args = args.shift
-      if args.class.to_s == 'Array' && args.count == 1
-        args = args.shift
-      end
-      case args.class.to_s
-      when 'Hash'
-        args.merge!('version' => 'latest') unless args.has_key?('version')
-        args.merge!('json' => 'default') unless args.has_key?('json')
-      when 'Array'
+      args = args.shift if args.class.to_s == 'Array' && args.count == 1
+      if args.class.to_s == 'Array'
         keys = ['type', 'name', 'version', 'json']
         args = Hash[keys.zip args]
       end
@@ -127,6 +191,13 @@ module Alfred
     #### GEM FUNCTIONS
 
     # Based on # # http://mlen.pl/posts/protip-installing-gems-programmatically/
+
+    #
+    # [install_gem description]
+    # @param name [type] [description]
+    # @param version = Gem::Requirement.default [type] [description]
+    #
+    # @return [type] [description]
     def install_gem(name, version = Gem::Requirement.default)
       require 'rubygems'
       require 'rubygems/dependency_installer'
@@ -136,6 +207,16 @@ module Alfred
       # end
     end
 
+
+    #
+    # [gems description]
+    # @overload gems(args)
+    #   @param hash
+    # @param *gems [type] [description]
+    #
+    # @raise -------- ?
+    #
+    # @return [type] [description]
     def gems(*gems)
       gems.each do |g|
         begin
@@ -154,7 +235,11 @@ module Alfred
     ######################
     #### ICON FUNCTIONS
 
-    # Function to get icons from the icon server
+    #
+    # [icon description]
+    # @param *args [type] [description]
+    #
+    # @return [type] [description]
     def icon(*args)
 
       if args.count == 1
@@ -233,6 +318,12 @@ module Alfred
       return fallback
     end
 
+
+    #
+    # [get_system_icon description]
+    # @param icon [type] [description]
+    #
+    # @return [type] [description]
     def get_system_icon(icon)
         # This is where the icon should be
         icon = "/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/#{icon}.icns";
@@ -249,12 +340,15 @@ module Alfred
     #######################
 
     # checks to see if string is a valid hex color
+
+    #
+    # [is_hex description]
+    # @param color [type] [description]
+    #
+    # @return [type] [description]
     def is_hex(color)
-      if /^[0-9a-f]{6}$|^[0-9a-f]{3}$/.match(color)
-        true
-      else
-        false
-      end
+      return true if /^[0-9a-f]{6}$|^[0-9a-f]{3}$/.match(color)
+      false
     end
 
     #######################
@@ -262,22 +356,35 @@ module Alfred
     #######################
 
     # converts 3 hex to 6 hex
+
+    #
+    # [convert_hex description]
+    # @param color [type] [description]
+    #
+    # @return [type] [description]
     def convert_hex(color)
-      if /^[0-9a-f]{6}$/.match(color)
-        color
-      elsif /^[0-9a-f]{3}$/.match(color)
+      return color if /^[0-9a-f]{6}$/.match(color)
+      if /^[0-9a-f]{3}$/.match(color)
         color[0] + color[0] + color[1] + color[1] + color[2] + color[2]
-      else
-        false
       end
+      false
     end
 
     # converts a hex number to a decimal
+
+    #
+    # [hex_to_dec description]
+    # @param color [type] [description]
+    #
+    # @return [type] [description]
     def hex_to_dec(color)
       color.map {|item| item.to_i(16)}
     end
 
     # converts a decimal to a hex number with a 0 pad
+    # @param color [type] [description]
+    #
+    # @return [type] [description]
     def dec_to_hex(color)
       hex = ""
       color.each do |x|
@@ -292,17 +399,31 @@ module Alfred
     end
 
     # converts hex to rgb
+    # @param hex [type] [description]
+    #
+    # @return [type] [description]
     def hex_to_rgb(hex)
-      hex = hex.scan(/.{2}/)
-      return hex_to_dec(hex)
+      return hex_to_dec(hex.scan(/.{2}/))
     end
 
     # converts rgb color to hex color
+
+    #
+    # [rgb_to_hex description]
+    # @param rgb [type] [description]
+    #
+    # @return [type] [description]
     def rgb_to_hex(rgb)
       return dec_to_hex(rgb)
     end
 
     # converts rgb to hsv colorspace
+
+    #
+    # [rgb_to_hsv description]
+    # @param rgb [type] [description]
+    #
+    # @return [type] [description]
     def rgb_to_hsv(rgb)
 
       r = rgb[0] / 1.0
@@ -314,15 +435,11 @@ module Alfred
       max = Float(rgb.max)
       chroma = max - min
 
-      if chroma == 0
-        return [0, 0, (max/255.0)]
-      end
+      return [0, 0, (max/255.0)] if chroma == 0
 
       if r == max
         h = (g - b)/chroma
-        if h < 0.0
-          h += 6.0;
-        end
+        h += 6.0 if h < 0.0
       elsif g == max
         h = ((b-r)/chroma) + 2.0
       else
@@ -336,14 +453,18 @@ module Alfred
       return [h,s,v]
     end
 
-    # converts hsv to rgb colorspace
+
+    #
+    # Converts an HSV colorpsace to RGB
+    # @param hsv array hsv values
+    #
+    # @return array rgb colorspace
     def hsv_to_rgb(hsv)
       h = Float(hsv[0])
       s = Float(hsv[1])
       v = Float(hsv[2])
-      r = 0
-      g = 0
-      b = 0
+
+      r, g, b = 0, 0, 0
 
       chroma = s * v
       h /= 60.0
@@ -371,36 +492,41 @@ module Alfred
         b = x
       end
 
-      r = ((r + min) * 255).round
-      g = ((g + min) * 255).round
-      b = ((b + min) * 255).round
-
-      return [r, g, b]
+      return [r, g, b].map { |x| ((x + min) * 255).round }
     end
 
+    #
     # Finds the luminance of a color space
     # I will take a hex or rgb color
+    # @param rgb [type] [description]
+    #
+    # @return [type] [description]
     def luminance(rgb)
-      if rgb.is_a? String
-        rgb = hex_to_rgb(rgb)
-      end
+      rgb = hex_to_rgb(rgb) if rgb.is_a? String
       return (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 255
     end
 
     # returns "light" or "dark" based on luminance
+
+    #
+    # [brightness description]
+    # @param color [type] [description]
+    #
+    # @return [type] [description]
     def brightness(color)
-      if luminance(color) > 0.5
-        return 'light'
-      else
-        return 'dark'
-      end
+      return 'light' if luminance(color) > 0.5
+      'dark'
     end
 
     # lightens or darkens a hex value or rgb space returning a hex
+
+    #
+    # [alter description]
+    # @param color [type] [description]
+    #
+    # @return [type] [description]
     def alter(color)
-      if color.is_a? String
-        color = hex_to_rgb(color)
-      end
+      color = hex_to_rgb(color) if color.is_a? String
       hsv = rgb_to_hsv(color)
       hsv = [hsv[0], hsv[1], 1.0 - hsv[2]]
       rgb = hsv_to_rgb(hsv)
@@ -414,51 +540,87 @@ module Alfred
     ######################
     #### LOG FUNCTIONS
 
-
-    # @TODO combine the log and file commands
-
+    #
+    # [initialize_logs description]
+    # @param user = false [type] [description]
+    #
+    # @return [type] [description]
     def initialize_logs( user = false )
-      self.initialize_bundler_log()
-      if user == true
-        self.initialize_user_log()
-      end
+      self.initialize_bundler_log
+      self.initialize_user_log if user == true
     end
 
+
+    #
+    # [init_log description]
+    # @param location = STDERR [type] [description]
+    #
+    # @return [type] [description]
     def init_log(location = STDERR)
+      # Check to see the type of log
       if location.is_a? String
+        # The log is a file, so let's make the path if it doesn't exist
         FileUtils.mkpath(File.dirname(location)) unless File.exists? File.dirname(location)
+        # Make and / or open the log file
         file = File.open(location, File::WRONLY | File::APPEND | File::CREAT)
+        # Create the log with only one backup and a max size of 1MB
         var  = Logger.new(file, 1, 1048576)
+        # Set the date format to YYYY-MM-DD HH:MM:SS
         date = "%Y-%m-%d %H:%M:%S"
       else
+        # The log is to STDOUT / STDERR (probably just the latter)
         var  = Logger.new(location)
+        # Set the date format to HH:MM:SS
         date = "%H:%M:%S"
       end
+      # We need to make the format match the bundler's
       var.formatter = proc do |severity, datetime, progname, msg|
+        # Get the last stack trace
         trace = caller.last.split(':')
+        # Get the filename from the stacktrace without the directory
         file = Pathname.new(trace[0]).basename
+        # Get the line number fron the trace
         line = trace[1]
+        date = Time.now.strftime("#{date}")
+        # Normalize the log levels to be in line with the rest of the bundler
+        severity = 'CRITICAL' if severity == 'FATAL'
+        severity = 'WARNING'  if severity == 'WARN'
 
-        if severity == 'FATAL'
-          severity = 'CRITICAL'
-        elsif severity == 'WARN'
-          severity = 'WARNING'
-        end
+        # Set the message format to the bundler standard
         "[#{date}] [#{file}:#{line}] [#{severity}] #{msg}\n"
       end
+      # Return the newly created log object
       return var
     end
 
+
+    #
+    # [initialize_user_log description]
+    #
+    # @return [type] [description]
     def initialize_user_log()
       @user = self.init_log(File.join(@wf_data, @bundle + '.log'))
     end
 
+
+    #
+    # [initialize_bundler_log description]
+    #
+    # @return [type] [description]
     def initialize_bundler_log()
+      # Create the bundler's log file
       log = File.join(@data, 'data', 'logs', 'bundler-' + @major_version + '.log')
       @file    = self.init_log(log)
-      @console = Logger.new(STDERR)
+      # Create the console log
+      @console = self.init_log(STDERR)
     end
 
+
+    #
+    # [fix_level description]
+    # @param level [type] [description]
+    #
+    # @return [type] [description]
     def fix_level(level)
       # Lowercase the level name
       level.downcase!
@@ -480,14 +642,26 @@ module Alfred
       return levels[level]
     end
 
+
+    #
+    # [log description]
+    # @param msg [type] [description]
+    # @param level = 'info' [type] [description]
+    #
+    # @return [type] [description]
     def log(msg, level = 'info' )
-      level = fix_level(level)
-      @file.send(level, msg)
+      @file.send(fix_level(level), msg)
     end
 
+
+    #
+    # [console description]
+    # @param msg [type] [description]
+    # @param level='info' [type] [description]
+    #
+    # @return [type] [description]
     def console(msg, level='info')
-      level = fix_level(level)
-      @console.send(level, msg)
+      @console.send(fix_level(level), msg)
     end
 
 
