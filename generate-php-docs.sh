28a86fcfecaeed3983d11c275fff50d61983c3bc
Shawn Patirck Rice <rice@shawnrice.org>
2014-08-22 20:31:06 -0400

    documentation stuff

---
+++ generate-php-docs.sh
@@ -0,0 +1,11 @@
+php exclude/apigen/apigen.php \
+--source bundler/AlfredBundler.php \
+--source bundler/includes/php-classes/AlfredBundlerIcon.php \
+--source bundler/includes/php-classes/AlfredBundlerLogger.php \
+--source bundler/bundlets/alfred.bundler.php \
+--source bundler/includes/wrappers/php/terminalnotifier.php \
+--source bundler/includes/wrappers/php/cocoadialog.php \
+--destination docs/php \
+--title 'Alfred Bundler' \
+--internal yes
+
