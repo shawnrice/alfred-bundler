831f272387fa14bfad4e784c7b17ac93e7840331
Shawn Patirck Rice <rice@shawnrice.org>
2014-09-01 16:10:24 -0500

    synced with devel

--- tests/ruby/start.rb
+++ tests/ruby/start.rb
@@ -1,37 +1,39 @@
 #!/bin/ruby
 
 
-exit
+
 
 ENV['AB_BRANCH'] = 'devel'
 require_relative File.expand_path( File.dirname(__FILE__) ) + "../../../bundler/bundlets/alfred.bundler.rb"
 
-
+bundler  = Alfred::Bundler.new
 
 begin
 bundler  = Alfred::Bundler.new
+
 rescue Alfred::BundlerInstallError => error
   raise StandardError.new("If you don't want to use the Bundler, uninstall this workflow") if error.reason == 'Deny'
   raise StandardError.new("We'll wait for you to try again later.") if error.reason == 'More Info'
 end
 
 
+puts bundler.utility({'name' => 'CocoaDialog', 'version' => '3.0.0-beta7'})
+puts bundler.utility(['Pashua', 'latest'])
+puts bundler.utility('Pashua', 'latest')
+puts bundler.utility('cocoaDialog')
+puts bundler.utility('viewer')
 
-# log = Alfred::Log.new(STDERR, Logger::DEBUG)
-# log.debug('test')
-# log.info('test')
-# bundler.notify('title', 'message', File.join(bundler.data, 'bundler', 'meta', 'icons', 'gem.png'))
+log = Alfred::Log.new(STDERR, Logger::DEBUG)
+log.debug('test')
+log.info('test')
 
-# exit
-# puts foo.inspect
-# foo.info('test')
-# puts bundler.load( 'utility', 'Pashua' )
-# puts bundler.load( 'php', 'Workflows' )
-# puts bundler.utility({'name' => 'CocoaDialog', 'version' => '3.0.0-beta7'})
-# puts bundler.utility(['Pashua', 'latest'])
-# puts bundler.utility('Pashua', 'latest')
-# puts bundler.utility('cocoaDialog')
-# puts bundler.utility('viewer')
+bundler.notify('title', 'message', File.join(bundler.data, 'bundler', 'meta', 'icons', 'gem.png'))
+
+
+
+puts bundler.load( 'utility', 'Pashua' )
+puts bundler.load( 'php', 'Workflows' )
+
 
 # bundler.foo
 # puts bundler.icon('font' => 'ELUSive', 'name' => 'fire', 'color' => 'abcabc', 'alter' => true)
@@ -44,7 +46,6 @@ end
 
 # [hex, rgb1, hsv, rgb2].each { |x| p x}
 
-# exit
 
 puts bundler.icon('elusive', 'fire', 'abcabc')
 
@@ -55,16 +56,16 @@ puts bundler.icon('elusive', 'fire', 'ffffff', true)
 puts bundler.icon('elusive', 'fire', 'abcabc', false)
 puts bundler.icon(['elusive', 'fire', 'abcabc'])
 puts bundler.icon({:font => 'elusive', :name => 'fire', :color => 'abcabc'})
-# puts bundler.icon('system', 'Accounts')
-# # puts bundler.icon('system', 'asda')
+puts bundler.icon('system', 'Accounts')
+# puts bundler.icon('system', 'asda')
 
-# # bundler.console("Test", 'inaasfo')
+# bundler.console("Test", 'inaasfo')
 # bundler.console("Fatal test", 'CRITICAL')
-# # bundler.log('Testing')
+# bundler.log('Testing')
 # bundler.log('Testing', 'FATAL')
 
 # # puts "We're about to load some gems"
-bundler.gems( ['oauth'], ['plist', '~>3.1.0'])
+# bundler.gems( ['oauth'], ['plist', '~>3.1.0'])
 # require 'plist'
 # p = Plist::Listener.new
 # p.tap{ |p| p p}
